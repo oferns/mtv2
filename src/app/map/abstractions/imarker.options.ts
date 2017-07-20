2f9dae322cb9ff8a556cb5e9df5f25f5eb00ec74
Ollie Ferns <oferns@gmail.com>
2017-07-20 16:47:27 +0100

    Defining common interface for marker interaciton

--- src/app/map/abstractions/imarker.options.ts
+++ src/app/map/abstractions/imarker.options.ts
@@ -1,4 +1,48 @@
 export interface IMarkerOptions {
-    
+    anchor: any;
+    draggable: boolean;
+    title: string;
+    label: string;
+    subTitle: string;
+    cursor: string;
+    icon: string;
+    onClick(handler: (...args: any[]) => void): void;
+    // onDragStart(handler: (...args: any[]) => void): void;
+    // onDrag(handler: (...args: any[]) => void): void;
+    // onDragEnd(handler: (...args: any[]) => void): void;
 
 }
+
+
+// Bing = google
+// anchor 
+// color
+// cursor
+// draggable
+// enableClickedStyle
+// enableHoverStyle
+// icon
+// roundClickableArea
+// subTitle
+// title
+// text
+// textOffset
+// visible
+
+
+// google
+// anchorPoint
+// animation
+// clickable
+// crossOnDrag
+// cursor
+// draggable
+// icon
+// label
+// opacity
+// optimized
+// place <- Leave this out
+// shape
+// title
+// visible
+// zIndex
